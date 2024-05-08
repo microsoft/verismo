1877c04e174e3cd6f026805d6475b776aa787fc2
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2024-05-08 05:11:27 +0000

    Add deps for kernel and python && wait 3 seconds before umount

--- tools/install.sh
+++ tools/install.sh
@@ -1,2 +1,2 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt-get install build-essential ninja-build libclang-dev libelf-dev qemu-system podman gcc-9
+sudo apt-get install build-essential ninja-build libclang-dev libelf-dev qemu-system podman gcc-9 cmake bison flex pip
