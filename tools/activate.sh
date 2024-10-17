d6d42cdaea5a494c8ef1911cc5bb3614d343304d
ziqiao zhou <ziqiaozhou@microsoft.com>
2024-10-17 04:32:06 +0000

    switch to github runner

--- tools/activate.sh
+++ tools/activate.sh
@@ -12,19 +12,15 @@ TOOLS_DIR=$(realpath $SCRIPT_DIR)
 
 echo "building verus-rustc."
 (
-  cargo install --path $TOOLS_DIR/cargo-v
-  cargo install --path $TOOLS_DIR/verus-rustc
-  cargo v prepare-verus
-  curl --proto '=https' --tlsv1.2 -LsSf https://github.com/verus-lang/verusfmt/releases/download/v0.4.3/verusfmt-installer.sh | sh
+  $TOOLS_DIR/vinstall.sh
 )
 
 echo "add igvm deps"
 (
-    python3 -m pip install frozendict
     git clone https://github.com/ziqiaozhou/igvm-tooling $TOOLS_DIR/igvm -b verismo-igvm
     cd "$TOOLS_DIR/igvm" && touch src/__init__.py
     if [ ! -f $TOOLS_DIR/venv ]; then
-    python3 -m venv $TOOLS_DIR/venv
+      python3 -m venv $TOOLS_DIR/venv
     fi
-    source $TOOLS_DIR/venv/bin/activate && python3 -m pip install src/
+    source $TOOLS_DIR/venv/bin/activate && python3 -m pip install frozendict && python3 -m pip install src/
 )
--- tools/vinstall.sh
+++ tools/vinstall.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-cargo install --git https://github.com/microsoft/verismo/ --rev 28dc242 cargo-v
-cargo v prepare-verus
-cargo install cargo-run-script
+# Determine the directory that this script is in
+if [ "$BASH_VERSION" ]; then
+  SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+elif [ "$ZSH_VERSION" ]; then
+  SCRIPT_DIR="$( cd "$( dirname "${(%):-%N}" )" >/dev/null 2>&1 && pwd )"
+else
+  echo "Unknown shell; exiting."
+  return 1
+fi
+TOOLS_DIR=$(realpath $SCRIPT_DIR)
+cargo install --path $TOOLS_DIR/cargo-v
+cargo install --path $TOOLS_DIR/verus-rustc
+cd source
+builtin=`cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "builtin_macros") | .targets[].src_path'`
+verus=`dirname $builtin`/../../../source/target-verus/release/verus
+if [ -f ${verus} ]; then
+echo "verus (${verus}) is already built"
+else
+cargo v prepare-verus 
+fi
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/verus-lang/verusfmt/releases/download/v0.4.3/verusfmt-installer.sh | sh
