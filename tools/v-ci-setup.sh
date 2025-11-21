7bed3bad605a38b270207f55d1b8b5a78e125d8a
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2025-11-21 01:35:55 +0000

    Fix broken apt install due to missing update

--- tools/v-ci-setup.sh
+++ tools/v-ci-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt-get install build-essential ninja-build libclang-dev libelf-dev qemu-system podman gcc-9 cmake bison flex unzip
+sudo apt update
+sudo apt install build-essential ninja-build libclang-dev libelf-dev qemu-system podman gcc-9 cmake bison flex unzip
 sudo apt install pip
 builtin=`cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "builtin") | .targets[].src_path'`
 verus=`dirname $builtin`/../../../
